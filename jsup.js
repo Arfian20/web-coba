aa7f9c352549f7397f9fe97264b07d841f3e719e
Arfian20 <arfisansurya20@gmail.com>
2021-06-07 11:19:52 +0700

    first commit

---
+++ jsup.js
@@ -0,0 +1,28 @@
+// const sMerah = document.querySelector(".sMerah");
+// const sHijau = document.querySelector(".sHijau");
+// const sBiru = document.querySelector(".sBiru");
+
+// sMerah.addEventListener(cursor, function () {
+//   const r = sMerah.value;
+//   const g = sHijau.value;
+//   const b = sBiru.value;
+//   document.body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+// });
+// sHijau.addEventListener(cursor, function () {
+//   const r = sMerah.value;
+//   const g = sHijau.value;
+//   const b = sBiru.value;
+//   document.body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+// });
+// sBiru.addEventListener(cursor, function () {
+//   const r = sMerah.value;
+//   const g = sHijau.value;
+//   const b = sBiru.value;
+//   document.body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+// });
+
+// document.body.addEventListener("mousemove", function (event) {
+//   const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+//   const yPos = Math.round((event.clientY / window.innerHeight) * 255);
+//   document.body.style.backgroundColor = "rgb(" + xPos + ", " + yPos + ", " + 100 + ")";
+// });
